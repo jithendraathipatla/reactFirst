7e2c83ebbb1e0001068aed97f18666686215a593
jithendraathipatla <athipatala.jithendra@gmail.com>
2019-08-09 16:57:09 +0530

    second commit

---
+++ src/Ajax.js
@@ -0,0 +1,69 @@
+
+import React from 'react';
+import { appendFile } from 'fs';
+var country = "";
+var response  = "";
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("https://o136z8hk40.execute-api.us-east-1.amazonaws.com/dev/get-list-of-conferences")
+      .then(res => res.json())
+        .then(
+          (result) => {
+            response = result.free;
+            this.setState({
+              isLoaded: true,
+              items: result.items
+            });
+        }
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          
+        )
+    }
+
+   
+   
+  
+    render() {
+       
+      const { error, isLoaded, items } = this.state;
+    
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+
+        const items = []
+        for(var i =0 ; i < response.length ; i++){
+            country = response[i].country;
+           var c = country;
+        for (const [index, value] of response.entries()) {
+          items.push(<li key={index}>{value}</li>)
+        }
+        }
+      
+       
+         return(
+               <div className="card-body">
+                  <h5 className="card-title">{items}</h5>
+                </div>
+           
+        );
+    
+      }
+    }
+  }
+
+  export default MyComponent
--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <div className="row">
+      <div className="col-sm-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+           
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+           
+          </div>
+        </div>
+      </div>
+    <div className="col-sm-4">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       
+      </div>
+    </div>
+  </div>
+</div>
       </header>
     </div>
   );
 }
 
 export default App;
+
